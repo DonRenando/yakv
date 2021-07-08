3040dda628c9f99ee13ad5fc655f5c19ac949eeb
burntcarrot <aadhav.n1@gmail.com>
2021-07-08 16:03:37 +0530

    fix: fix install.sh

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/burntcarrot/yakv
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.7.2
+require (
+	github.com/gin-gonic/gin v1.7.2
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
+)
--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ ENDCOLOR="\033[0m"
 echo -e "${CYAN}"
 echo "yakv is a simple, in-memory key-value store for hobbyists."
 echo "-----------------------"
-echo "Installing yakv v0.1.0"
+echo "Installing yakv v0.1.1"
 echo "-----------------------"
 echo -n -e "${ENDCOLOR}"
 
-yakv_linux_url=https://github.com/burntcarrot/yakv/releases/download/v0.1.0/yakv-0.1.0-linux-amd64
+yakv_linux_url=https://github.com/burntcarrot/yakv/releases/download/v0.1.1/yakv-0.1.1-linux-amd64
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo -e "${YELLOW}"
@@ -21,7 +21,7 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         echo "Fetching the latest release for Linux...."
         echo -n -e "${ENDCOLOR}"
         curl -LJO $yakv_linux_url
-        sudo mv ./yakv-0.1.0-linux-amd64 ./yakv
+        sudo mv ./yakv-0.1.1-linux-amd64 ./yakv
         echo ""
         echo -e "${YELLOW}"
         echo "Installing binary...."
